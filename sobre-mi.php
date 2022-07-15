c11e3a6bf1fd51c8eebd17cada65c01d3d67aff0
gastonvasquez <108307165+gastonvasquez@users.noreply.github.com>
2022-07-15 16:35:11 -0300

    Update sobre-mi.php

--- sobre-mi.php
+++ sobre-mi.php
@@ -245,8 +245,7 @@
             <div class="col-12 col-sm-3 text-center text-sm-start pt-2 p-sm-0">
                 <a href="mailto:info@nelsontarche.com.ar">info@nelsontarche.com.ar</a>
             </div>
-        </div>
-      </footer>
+       </div>
        <div class="whatsapp">
         <a href="https://api.whatsapp.com/send?phone=541162423341" title="Whatsapp"><i class="fab fa-whatsapp"></i></a>
         </div>
